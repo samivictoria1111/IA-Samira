f6b48644662efe18e79314b655c7512d49445576
Sami Victoria <119086545+samivictoria1111@users.noreply.github.com>
2024-05-28 19:39:05 -0300

    Add files via upload

---
+++ JS script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector ('.caixa-principal');
+const caixaPergunta = document.querySelector ('.caixa-pergunta');
+const caixaAlternativas = document.querySelector ('.caixa-alternativas');
+const caixaResultado = document.querySelector ('.caixa-resultado');
+const caixaResultado = document.querySelector ('.caixa-resultado');
